8541c9f3752998be239c49f9a4f411a0efeefe17
user <user@users-MacBook-Pro.local>
2024-10-20 08:11:44 -0700

    pag

--- src/components/listArticles/listArticles.jsx
+++ src/components/listArticles/listArticles.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Pagination } from 'antd'
 import { useNavigate } from 'react-router-dom'
 
 import formatDate from '../../services/formatDate'
 import TagList from '../tagList/tagList'
+import useGetArticles from '../../services/getArticles'
 
 import styles from './listArticles.module.css'
 
 const ListArticles = () => {
   const articles = useSelector(state => state.articles)
+  const countArticles = useSelector(state => state.countArticles)
+
   const navigate = useNavigate()
+
+  const [currentPage, setCurrentPage] = useState(1)
+  const pageSize = 20
+
   const handleClick = slug => {
     navigate(`/${slug}`)
   }
 
+  // Вызов хука для загрузки статей с текущими параметрами
+  useGetArticles(pageSize, (currentPage - 1) * pageSize)
+
+  const handlePageChange = page => {
+    setCurrentPage(page)
+  }
+
   const renderArticles = article => {
+    if (!article || !article.title || !article.body || !article.author) {
+      return null // Или вернуть сообщение об ошибке
+    }
     const { title, tagList, body, slug, author, createdAt, favoritesCount } = article
+
     const { username, image } = author
 
     const truncatedBody = body.length > 150 ? `${body.substring(0, 150)}...` : body
@@ -34,7 +52,7 @@ const ListArticles = () => {
           <TagList tagList={tagList} />
           <div className={styles.text}>{truncatedBody}</div>
         </div>
-        <div className={styles.containerАuthor}>
+        <div className={styles.containerAuthor}>
           <div className={styles.containerFlex}>
             <div className={styles.name}>{username}</div>
             <div className={styles.dateCreate}>{formatDate(createdAt)}</div>
@@ -48,7 +66,15 @@ const ListArticles = () => {
   return (
     <>
       {articles.map(article => renderArticles(article))}
-      <Pagination defaultCurrent={1} total={50} className={styles.paginationArticles} align="center" />
+      <Pagination
+        current={currentPage} // Указываем текущую страницу
+        total={countArticles}
+        pageSize={pageSize} // Указываем размер страницы
+        className={styles.paginationArticles}
+        onChange={handlePageChange}
+        align="center"
+        showSizeChanger={false}
+      />
     </>
   )
 }
