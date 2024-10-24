88efbe351523135c086e8d052a10643f5d39dfa9
user <user@users-MacBook-Pro.local>
2024-10-24 15:41:58 -0700

    validation

--- src/components/signUpForm/signUpform.jsx
+++ src/components/signUpForm/signUpform.jsx
@@ -1,38 +1,91 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import registerUsers from '../../services/registerUser'
 
 import styles from './signUpForm.module.css'
 
 const SignUpForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordRepeat, setPasswordRepeat] = useState('')
+  const [email, setEmail] = useState('')
+  const [isCheckboxAgreement, setisCheckboxAgreement] = useState(false)
+  const [errorPassword, setErrorPassword] = useState('')
+  const [errorAgreement, setErrorAgreement] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (password.length < 7 || password.length > 40) {
+      setErrorPassword('Пароль должен быть от 7 до 40 символов')
+
+      return
+    }
+
+    if (password !== passwordRepeat) {
+      setErrorPassword('Пароли не совпадают')
+
+      return
+    }
+
+    if (!isCheckboxAgreement) {
+      setErrorAgreement('Вы должны согласиться с обработкой личной информации')
+
+      return
+    }
+
+    setErrorPassword('')
+    setErrorAgreement('')
+
+    registerUsers(username, password, email)
+  }
+
   return (
-    <div className={styles.signUpForm}>
+    <form className={styles.signUpForm} onSubmit={handleSubmit}>
       <div className={styles.newAccount}>Create new account</div>
       <div className={styles.loginDetails}>
         <div className={styles.userName}>
           <div>Username</div>
-          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
         </div>
 
         <div className={styles.emailAddress}>
           <div>Email address</div>
-          <input type="text" placeholder="Email address" />
+          <input type="email" placeholder="Email address" value={email} onChange={e => setEmail(e.target.value)} />
         </div>
 
         <div className={styles.password}>
           <div>Password</div>
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+          {errorPassword && <div className={styles.errorMessage}>{errorPassword}</div>}
         </div>
 
         <div className={styles.passwordAgain}>
           <div>Repeat Password</div>
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            value={passwordRepeat}
+            onChange={e => setPasswordRepeat(e.target.value)}
+          />
         </div>
       </div>
 
-      <div>
+      <div className={styles.agreementContainer}>
         <p className={styles.agreementText}>I agree to the processing of my personal information</p>
+        <input
+          type="checkbox"
+          className={styles.agreementCheckbox}
+          checked={isCheckboxAgreement}
+          onChange={e => setisCheckboxAgreement(e.target.checked)}
+        />
+        <div>{errorAgreement && <div className={styles.errorMessage}>{errorAgreement}</div>}</div>
       </div>
 
-      <button className={styles.create}>Create</button>
+      <button type="submit" className={styles.create}>
+        Create
+      </button>
 
       <p className={styles.alreadyHaveAccount}>
         Already have an account?{' '}
@@ -41,7 +94,7 @@ const SignUpForm = () => {
         </Link>
         .
       </p>
-    </div>
+    </form>
   )
 }
 
---
+++ src/services/registerUser.js
@@ -0,0 +1,28 @@
+const registerUsers = async (username, password, email) => {
+  try {
+    const response = await fetch('https://blog-platform.kata.academy/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          email,
+          password,
+        },
+      }),
+    })
+
+    const data = await response.json()
+    if (response.ok) {
+      console.log('User registered:', data)
+    } else {
+      console.error('Error:', data) // Вывод ошибки с сервера
+    }
+  } catch (err) {
+    console.error('Network error:', err)
+  }
+}
+
+export default registerUsers
